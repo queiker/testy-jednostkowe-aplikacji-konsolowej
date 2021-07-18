9aab088d77f613168672c8196ae79174c64818cb
Grzegorz Płonka Kraków ul.Źródlana 11 30-734 <tradecomp.pl@gmail.com>
2021-07-18 11:52:53 +0200

    dodano projekt z jednym podstawą asercją

---
+++ Kalkulator_testy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kalkulator_testy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
